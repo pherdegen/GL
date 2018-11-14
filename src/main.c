feffacb2fe0f32a80f831601cc2145372cd433db
pherdegen <paul.herdegen@u-psud.fr>
2018-11-14 22:23:13 +0100

    Ex 44

--- src/main.c
+++ src/main.c
@@ -1,26 +1,16 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+//EX 44//
 
 #include "my_header.h"
-#include <stdio.h>
-#include <math.h>
-
-#define ECART 1e-10
+#include "TP_lib.h"
 
 
-int main(void)
+void main(void)
 {
-    double a = 2.0, b = 3.0, c = 4.0, delta;
-    unsigned char nb_solutions;
+	short resultat , i=0;
 
-    delta = b*b - 4*a*c;
-
-    if( fabs(delta) < ECART )
-        nb_solutions = 1;
-    else if (delta > 0.0)
-        nb_solutions = 2;
-    else
-        nb_solutions = 0;
-
-        return 0;
+	while(i<10)
+	{
+		resultat=LancerDe();
+		i=i+1;
+	}
 }
