f91d1a4b8d48d0e4c8d1ce4f5f80a5f5895c6c27
paul herdegen <paul.herdegen@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:31:29 +0100

    Ex56

--- src/main.c
+++ src/main.c
@@ -2,8 +2,22 @@
 //
 
 #include "my_header.h"
+#define Seuil 50
 
 int main(void)
 {
-        return 0;
+	unsigned char bin[10] = {5, 15, 25, 35, 45, 55, 65, 75, 85, 95};
+	int i;
+	
+	for(i=0; i<10; i++)
+	{
+		if (bin[i]<Seuil)
+		{
+			bin[i]=0;
+		}
+		else
+		{
+			bin[i]=1;
+		} 
+	}return 0;
 }
