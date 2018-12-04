deb7b816eb3e6c6000c7ffa7f88a9fbb80ef07af
paul herdegen <paul.herdegen@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:33:44 +0100

    Finis

--- src/main.c
+++ src/main.c
@@ -5,22 +5,21 @@
 #include <stdio.h>
 #include <math.h>
 
-#define ECART 1e-10
 
 
 int main(void)
 {
-    double a = 2.0, b = 3.0, c = 4.0, delta;
-    unsigned char nb_solutions;
+    char nom[100]; 
+	char age[100]; 
+	char prenom[100];
+	
+	printf("Tapez le nom : ");
+	scanf ("%s", nom);
+	printf("Tapez le prenom : ");
+	scanf ("%s", prenom);
+	printf("Tapez l'age : ");
+	scanf ("%s", age);
+	
+	printf("Je m'appelle %s %s et j'ai %s ans.", nom, prenom, age);
 
-    delta = b*b - 4*a*c;
-
-    if( fabs(delta) < ECART )
-        nb_solutions = 1;
-    else if (delta > 0.0)
-        nb_solutions = 2;
-    else
-        nb_solutions = 0;
-
-        return 0;
 }
